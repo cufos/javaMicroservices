4f169521cc46fa25833981115e52c41aef05ba92
yunior <yunior.varon@yahoo.com>
2023-01-20 16:18:00 +0100

    starting with spring microservices

---
+++ src/test/java/com/pizzarestaurant/store/PizzaRestaurantServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pizzarestaurant.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaRestaurantServiceApplicationTests {
+
+	@Test
+	void populateDB() {
+
+
+	}
+
+}
