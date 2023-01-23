34f592be9be61016626cc6cd58743e8ace958b39
yunior <yunior.varon@yahoo.com>
2023-01-23 10:47:45 +0100

    Created the controller with his implementation

---
+++ src/main/java/com/pizzarestaurant/store/controller/PizzaController.java
@@ -0,0 +1,28 @@
+package com.pizzarestaurant.store.controller;
+
+import com.pizzarestaurant.store.dto.PizzaDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Api(tags = "Pizza API")
+public interface PizzaController {
+
+  @ApiOperation("Add new pizza")
+  public PizzaDTO save(@RequestBody PizzaDTO pizzaDTO);
+
+  @ApiOperation("Find pizza by id")
+  public PizzaDTO findById(@PathVariable("id") Long id);
+
+  @ApiOperation("Delete by id")
+  public void deleteById(@PathVariable("id") Long id);
+
+  @ApiOperation("Find all pizzas")
+  public List<PizzaDTO> list();
+
+  @ApiOperation("Update Pizza")
+  public PizzaDTO update(@RequestBody PizzaDTO pizza, @PathVariable("id") Long id);
+}
