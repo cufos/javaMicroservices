d55fb3d269326d4b4b0393336fa9815ade3cc437
yunior <yunior.varon@yahoo.com>
2023-01-23 13:07:50 +0100

    Created the dto, repository,service, and the mappers with all implementation for the drivers and the restaurants

---
+++ src/main/java/com/pizzarestaurant/store/service/RestaurantService.java
@@ -0,0 +1,6 @@
+package com.pizzarestaurant.store.service;
+
+import com.pizzarestaurant.store.model.Restaurant;
+
+public interface RestaurantService extends GenericService<Restaurant,Long> {
+}
--- src/main/java/com/pizzarestaurant/store/service/impl/PizzaServiceImpl.java
+++ src/main/java/com/pizzarestaurant/store/service/impl/PizzaServiceImpl.java
@@ -49,4 +49,5 @@ public class PizzaServiceImpl implements PizzaService {
     }
     return null;
   }
+
 }
