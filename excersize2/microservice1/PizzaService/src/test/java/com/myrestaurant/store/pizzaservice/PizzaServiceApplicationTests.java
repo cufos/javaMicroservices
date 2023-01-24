1627642742d26766532698a9e525733c7f34fd17
yunior <yunior.varon@yahoo.com>
2023-01-24 10:08:56 +0100

    Changed all the project directory and file system

---
+++ excersize2/microservice1/PizzaService/src/test/java/com/myrestaurant/store/pizzaservice/PizzaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myrestaurant.store.pizzaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
